1dc43b877ed053da481c161d28e3b80daaf6b925
esmameral <esma.meral@gmail.com>
2023-02-21 12:00:13 +0300

    first project

---
+++ CS105Examples/src/demo/Hello.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int firstValue=19;
+		int secondValue=45;
+		
+		int sum=firstValue+secondValue;
+		System.out.println(sum);
+
+		
+	}
+
+}
