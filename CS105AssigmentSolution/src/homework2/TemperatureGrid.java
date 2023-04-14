edfa456f31bbd001bb042b5ec1d879439dff2d11
esmameral <esma.meral@gmail.com>
2023-04-14 13:11:59 +0300

    Solution of Homework2 added

--- CS105AssigmentSolution/src/homework2/TemperatureGrid.java
+++ CS105AssigmentSolution/src/homework2/TemperatureGrid.java
@@ -6,6 +6,14 @@ public class TemperatureGrid {
 	 * (not shown) Guaranteed not to be null
 	 */
 	private double[][] temps;
+	private double[][] updatedTemps;
+
+	public TemperatureGrid(double[][] t) {
+		super();
+		temps = t;
+		updatedTemps=t;
+
+	}
 
 	/**
 	 * Computes and returns a new temperature value for the given location.
@@ -18,9 +26,16 @@ public class TemperatureGrid {
 	 *         four adjacent entries. Postcondition: temps is unchanged.
 	 */
 	private double computeTemp(int row, int col) {
-		/* to be implemented in part (a) */ 
-		return 0;
+		double temp1 = temps[row][col];
+
+		if (row == 0 || col == 0 || row == temps.length - 1 || col == temps[0].length - 1)
+			return temp1;
+		else {
+			double avg = temps[row][col - 1] + temps[row][col + 1] + temps[row - 1][col] + temps[row + 1][col];
+			avg /= 4;
+			return avg;
 		}
+	}
 
 	/**
 	 * Updates all values in temps and returns a boolean that indicates whether or
@@ -33,8 +48,35 @@ public class TemperatureGrid {
 	 *         computeTemp.
 	 */
 	public boolean updateAllTemps(double tolerance) {
-		/* to be implemented in part (b) */ 
-		return false;
+		updatedTemps = new double[temps.length][temps[0].length];
+		int falsecount = 0;
+
+		for (int i = 0; i < temps.length; i++) {
+			for (int j = 0; j < temps[0].length; j++) {
+				updatedTemps[i][j] = computeTemp(i, j);
+				if (Math.abs(updatedTemps[i][j] - temps[i][j]) > tolerance)
+					falsecount++;
+
+			}
 		}
+		if (falsecount >= 1)
+			return false;
+		else
+			return true;
+	}
+
+	public String toString() {
+		String output = "";
+
+		for (int i = 0; i < temps.length; i++) {
+			for (int j = 0; j < temps[0].length; j++) {
+				output += (updatedTemps[i][j] + "\t");
+			}
+			output += ("\n");
+		}
+
+		return output;
+	}
+
 // There may be instance variables, constructors, and methods that are not shown.
 }
---
+++ CS105AssigmentSolution/src/homework2/TemperatureTest.java
@@ -0,0 +1,13 @@
+package homework2;
+
+public class TemperatureTest {
+
+	public static void main(String[] args) {
+		double[][] temps = { { 95.5, 100.0, 100.0, 100.0, 100.0, 110.3 }, { 0, 50, 50, 50, 50, 0 },
+				{ 0, 40, 40, 40, 40, 0 }, { 0, 20, 20, 20, 20, 0 }, { 0, 0, 0, 0, 0, 0 } };
+		TemperatureGrid grid = new TemperatureGrid(temps);
+		System.out.println(grid.updateAllTemps(0.01));
+		System.out.println(grid);
+	}
+
+}
